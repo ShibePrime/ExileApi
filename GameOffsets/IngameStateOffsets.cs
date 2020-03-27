4d0025b15d79e6cb279e2ec673760ec6ac0b04e6
Queuete <->
2020-03-27 23:30:58 +0100

    Minor fixes

--- GameOffsets/IngameStateOffsets.cs
+++ GameOffsets/IngameStateOffsets.cs
@@ -14,19 +14,18 @@ namespace GameOffsets
         [FieldOffset(0x4F0)] public long UIHover;
         [FieldOffset(0x524)] public float UIHoverX;
         [FieldOffset(0x528)] public float UIHoverY;
-		[FieldOffset(0x52C)] public float MouseXInGame;
         [FieldOffset(0x4E8)] public float CurentUElementPosX;
         [FieldOffset(0x4EC)] public float CurentUElementPosY;
 		[FieldOffset(0x55C)] public float TimeInGameF;
         [FieldOffset(0x570)] public int DiagnosticInfoType;
-        [FieldOffset(0x7A0)] public long LatencyRectangle;
-        [FieldOffset(0xC40)] public long FrameTimeRectangle;
-        [FieldOffset(0xE90)] public long FPSRectangle;
         [FieldOffset(0x554)] public float TimeInGame;
-        [FieldOffset(0xFE4)] public int Camera;
         [FieldOffset(0x518)] public int MouseXGlobal;
         [FieldOffset(0x51C)] public int MouseYGlobal;
         [FieldOffset(0x524)] public float MouseXInGame;
         [FieldOffset(0x528)] public float MouseYInGame;
+        [FieldOffset(0x7A0)] public long LatencyRectangle;
+        [FieldOffset(0xC40)] public long FrameTimeRectangle;
+        [FieldOffset(0xE90)] public long FPSRectangle;
+        [FieldOffset(0xFE4)] public int Camera;
     }
 }
