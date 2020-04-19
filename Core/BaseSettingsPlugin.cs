3dfb38061851f280f0b39796d4fc2496cddd696f
Queuete <->
2020-04-19 09:12:04 +0200

    Add DebugObjects to BaseSettingsPlugin and update DevTree to display it

--- Core/BaseSettingsPlugin.cs
+++ Core/BaseSettingsPlugin.cs
@@ -21,8 +21,9 @@ namespace ExileCore
             InternalName = GetType().Namespace;
             if (string.IsNullOrWhiteSpace(Name)) Name = InternalName;
             Drawers = new List<ISettingsHolder>();
+            DebugObjects = new Dictionary<string, object>();
         }
-
+        public static Dictionary<string, object> DebugObjects { get; set; }
         public List<ISettingsHolder> Drawers { get; }
         public GameController GameController { get; private set; }
         public Graphics Graphics { get; private set; }
@@ -59,6 +60,11 @@ namespace ExileCore
                 throw new NullReferenceException("Plugin settings is null");
 
             GameController.Settings.SaveSettings(this);
+        }
+
+        public static void AddDebugObject(string key, object obj)
+        {
+            DebugObjects[key] = obj;
         }
 
         public virtual void AreaChange(AreaInstance area)
