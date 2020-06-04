639d631861b3694b1fbe099efb80df13dfc3fca9
Queuete <->
2020-06-04 12:02:48 +0200

    Move VersionResult enum to separate file

--- Core/Shared/VersionChecker/VersionChecker.cs
+++ Core/Shared/VersionChecker/VersionChecker.cs
@@ -9,24 +9,6 @@ using System.Threading.Tasks;
 
 namespace ExileCore.Shared.VersionChecker
 {
-    public enum VersionResult
-    {
-        Loading,
-        UpToDate,
-        MajorUpdate,
-        MinorUpdate,
-        PatchUpdate,
-        Error
-    }
-    
-    static class VersionResultExtension
-    {
-        public static bool IsUpdate(this VersionResult vr)
-        {
-            return vr == VersionResult.MajorUpdate || vr == VersionResult.MinorUpdate || vr == VersionResult.PatchUpdate;
-        }
-    }
-
     public class VersionChecker
     {
         public const string VERSION_FILE_NAME = "version.json";
---
+++ Core/Shared/VersionChecker/VersionResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExileCore.Shared.VersionChecker
+{
+    public enum VersionResult
+    {
+        Loading,
+        UpToDate,
+        MajorUpdate,
+        MinorUpdate,
+        PatchUpdate,
+        Error
+    }
+
+    public static class VersionResultExtension
+    {
+        public static bool IsUpdate(this VersionResult vr)
+        {
+            return vr == VersionResult.MajorUpdate || vr == VersionResult.MinorUpdate || vr == VersionResult.PatchUpdate;
+        }
+    }
+}
