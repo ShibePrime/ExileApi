3490a9e4048dd8588f45dfacaed70ce6f60c5293
MM <meyerm1@gmail.com>
2020-09-19 14:56:07 +0200

    fix some offsets for 3.12

--- GameOffsets/CameraOffsets.cs
+++ GameOffsets/CameraOffsets.cs
@@ -8,11 +8,11 @@ namespace GameOffsets
     {
         [FieldOffset(0x8)] public int Width;
         [FieldOffset(0xC)] public int Height;
-        [FieldOffset(0x214)] public float ZFar;
+        [FieldOffset(0x1C4)] public float ZFar;
 
         //First value is changing when we change the screen size (ratio)
         //4 bytes before the matrix doesn't change
-        [FieldOffset(0xA8)] public Matrix MatrixBytes;
-		[FieldOffset(0x120)] public Vector3 Position;
+        [FieldOffset(0x80)] public Matrix MatrixBytes;
+        [FieldOffset(0xF0)] public Vector3 Position;
     }
 }
--- GameOffsets/IngameDataOffsets.cs
+++ GameOffsets/IngameDataOffsets.cs
@@ -7,13 +7,13 @@ namespace GameOffsets
     public struct IngameDataOffsets
     {
         [FieldOffset(0x60)] public long CurrentArea;
-        [FieldOffset(0x78)] public byte CurrentAreaLevel;
+        [FieldOffset(0x80)] public byte CurrentAreaLevel;
         [FieldOffset(0xF0)] public NativePtrArray MapStats;
-        [FieldOffset(0xDC)] public uint CurrentAreaHash;
-        [FieldOffset(0x400)] public long LocalPlayer;
+        [FieldOffset(0xE4)] public uint CurrentAreaHash;
+        [FieldOffset(0x408)] public long LocalPlayer;
         [FieldOffset(0x11C)] public long LabDataPtr;
-        [FieldOffset(0x488)] public long EntityList;
-        [FieldOffset(0x490)] public long EntitiesCount;
-        [FieldOffset(0x608)] public TerrainData Terrain;
+        [FieldOffset(0x490)] public long EntityList;
+        [FieldOffset(0x498)] public long EntitiesCount;
+        [FieldOffset(0x610)] public TerrainData Terrain;
     }
 }
