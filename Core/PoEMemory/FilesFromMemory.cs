e2e8c88c68ca829e667c6a06e3f04b5dcb28e8ee
Snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2020-09-29 01:57:08 -0700

    Restored two fonts purged in merger and other things.

--- Core/PoEMemory/FilesFromMemory.cs
+++ Core/PoEMemory/FilesFromMemory.cs
@@ -33,11 +33,11 @@ namespace ExileCore.PoEMemory
         public Dictionary<string, FileInformation> GetAllFiles()
         {
             var files = new ConcurrentDictionary<string, FileInformation>();
-            var addressOfProcess = _mem.AddressOfProcess + _mem.BaseOffsets[OffsetsName.FileRoot];
+            var fileRootAddress = _mem.AddressOfProcess + _mem.BaseOffsets[OffsetsName.FileRoot];
             Parallel.For(0, 256, i => {
-                var readAddress = addressOfProcess + i * 0x40;
-                var fileChunkStruct = _mem.Read<FilesOffsets>(readAddress);
-                ReadDictionary(fileChunkStruct.ListPtr, files);
+                var fileAddress = fileRootAddress + i * 0x40;
+                var fileChunk = _mem.Read<FilesOffsets>(fileAddress);
+                ReadDictionary(fileChunk.ListPtr, files);
             });
             return files.ToDictionary();
         }
@@ -46,13 +46,12 @@ namespace ExileCore.PoEMemory
         {
             var node = _mem.Read<FileNode>(head);
             var sw = Stopwatch.StartNew();
-            var headLong = head;
-
-            while (headLong != node.Next)
+            
+            while (head != node.Next)
             {
                 if (sw.ElapsedMilliseconds > 2000)
                 {
-                    DebugWindow.LogError($"ReadDictionary error. Elapsed: {sw.ElapsedMilliseconds}");
+                    DebugWindow.LogError($"FilesFromMemory -> ReadDictionary(...) timeout error. Elapsed: {sw.ElapsedMilliseconds}");
                     break;
                 }
 
@@ -60,8 +59,8 @@ namespace ExileCore.PoEMemory
                 
                 if (!dictionary.TryGetValue(key, out _))
                 {
-                    var changeCount = _mem.Read<int>(node.Value + 0x38);
-                    dictionary[key] = new FileInformation(node.Value, changeCount);
+                    var areaChangeCount = _mem.Read<int>(node.Value + 0x38);
+                    dictionary[key] = new FileInformation(node.Value, areaChangeCount);
                 }
 
                 node = _mem.Read<FileNode>(node.Next);
@@ -71,11 +70,11 @@ namespace ExileCore.PoEMemory
         public Dictionary<string, FileInformation> GetAllFilesSync()
         {
             var files = new ConcurrentDictionary<string, FileInformation>();
-            var addressOfProcess = _mem.AddressOfProcess + _mem.BaseOffsets[OffsetsName.FileRoot];
+            var fileRootAddress = _mem.AddressOfProcess + _mem.BaseOffsets[OffsetsName.FileRoot];
             Parallel.For(0, 256, i => {
-                var readAddress = addressOfProcess + i * 0x40;
-                var fileChunkStruct = _mem.Read<FilesOffsets>(readAddress);
-                ReadDictionary(fileChunkStruct.ListPtr, files);
+                var fileAddress = fileRootAddress + i * 0x40;
+                var fileChunk = _mem.Read<FilesOffsets>(fileAddress);
+                ReadDictionary(fileChunk.ListPtr, files);
             });
             return files.ToDictionary();
         }
