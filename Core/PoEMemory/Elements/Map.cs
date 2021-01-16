c7b0fc157dc0ac110e851e2b78f5eb0ef0861800
Queuete <->
2021-01-16 14:46:59 +0100

    Fix LargeMap offset

--- Core/PoEMemory/Elements/Map.cs
+++ Core/PoEMemory/Elements/Map.cs
@@ -6,12 +6,12 @@ namespace ExileCore.PoEMemory.Elements
         private Element _largeMap;
         private Element _smallMap;
         public Element LargeMap => _largeMap ?? (_largeMap = ReadObjectAt<Element>(0x230));
-        public float LargeMapShiftX => M.Read<float>(LargeMap.Address + 0x1C0);
-        public float LargeMapShiftY => M.Read<float>(LargeMap.Address + 0x1C4);
+        public float LargeMapShiftX => M.Read<float>(LargeMap.Address + 0x200);
+        public float LargeMapShiftY => M.Read<float>(LargeMap.Address + 0x204);
         public float LargeMapZoom => M.Read<float>(LargeMap.Address + 0x244);
         public Element SmallMiniMap => _smallMap ?? (_smallMap = ReadObjectAt<Element>(0x238));
-        public float SmallMinMapX => M.Read<float>(SmallMiniMap.Address + 0x1C0);
-        public float SmallMinMapY => M.Read<float>(SmallMiniMap.Address + 0x1C4);
+        public float SmallMinMapX => M.Read<float>(SmallMiniMap.Address + 0x200);
+        public float SmallMinMapY => M.Read<float>(SmallMiniMap.Address + 0x204);
         public float SmallMinMapZoom => M.Read<float>(SmallMiniMap.Address + 0x244);
         public Element OrangeWords => ReadObjectAt<Element>(0x250);
         public Element BlueWords => ReadObjectAt<Element>(0x2A8);
