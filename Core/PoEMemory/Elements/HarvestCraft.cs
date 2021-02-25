e3e0166692dee1c0a229d379fd45489e6a789d2e
arturino009 <arturino009@inbox.lv>
2021-02-25 18:28:38 +0200

    Added some text formatting

--- Core/PoEMemory/Elements/HarvestCraft.cs
+++ Core/PoEMemory/Elements/HarvestCraft.cs
@@ -4,8 +4,25 @@
     {
         override public string Text => GetChildAtIndex(3).Text;
 
+        public string ParsedName => removeRandomFormatting(GetChildAtIndex(3).Text);
+
         public string Count => GetChildAtIndex(1).GetChildAtIndex(0).Text;
 
         public string Level => GetChildAtIndex(4).Text;
+
+        private string removeRandomFormatting(string text)
+        {
+            if (text.IndexOf('<') != -1) //removing text coloring property (things like <white> and <craftcolor>)
+            {
+                int start = text.IndexOf('<');
+                int end = text.IndexOf('>', start) + 1;
+                string result = text.Remove(start, end - start);
+                text = removeRandomFormatting(result);
+            }
+            text = text.Replace("{", "");
+            text = text.Replace("}", "");
+            return text;
+        }
+
     }
 }
