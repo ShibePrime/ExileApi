cfaf34e7aced1f86b3e139029c496c1180dcc2ce
arturino009 <arturino009@inbox.lv>
2021-02-25 12:39:20 +0200

    Implement harvest window

---
+++ Core/PoEMemory/Elements/HarvestCraft.cs
@@ -0,0 +1,11 @@
+﻿namespace ExileCore.PoEMemory.Elements
+{
+    public class HarvestCraft : Element
+    {
+        override public string Text => GetChildAtIndex(3).Text;
+
+        public string Count => GetChildAtIndex(1).GetChildAtIndex(0).Text;
+
+        public string Level => GetChildAtIndex(4).Text;
+    }
+}
---
+++ Core/PoEMemory/MemoryObjects/HarvestWindow.cs
@@ -0,0 +1,12 @@
+﻿using ExileCore.PoEMemory.Elements;
+using System.Collections.Generic;
+
+namespace ExileCore.PoEMemory.MemoryObjects
+{
+    public class HarvestWindow : Element
+    {
+
+        public List<HarvestCraft> Crafts => GetChildFromIndices(8, 0, 1).GetChildrenAs<HarvestCraft>();
+
+    }
+}
