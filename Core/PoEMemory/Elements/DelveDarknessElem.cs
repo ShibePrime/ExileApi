8a47a8d56540f5421deeb929879056b56078513e
wlastas <wlastas@mail.ru>
2021-04-08 14:57:02 +0300

    Update DelveDarknessElem.cs

--- Core/PoEMemory/Elements/DelveDarknessElem.cs
+++ Core/PoEMemory/Elements/DelveDarknessElem.cs
@@ -1,5 +1,5 @@
 namespace ExileCore.PoEMemory.Elements {
-    public class DelveDarknessElem :Element { 
+    public class DelveDarknessElement :Element { 
         public int darkness { get { var elem = GetChildFromIndices(0, 0, 2, 0);
                 return elem == null || (elem!=null && elem.Text==null)  ? 0 : int.Parse(elem.Text);
             } }
