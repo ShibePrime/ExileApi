2b58de2172db1d8b41806a2868ce770b50a4510d
wlastas <wlastas@mail.ru>
2021-04-08 11:21:56 +0300

    Update IngameUElementsOffsets.cs

--- GameOffsets/IngameUElementsOffsets.cs
+++ GameOffsets/IngameUElementsOffsets.cs
@@ -116,8 +116,10 @@ namespace GameOffsets
         //[FieldOffset(0xA00)] public long HeistNotifyPanel;
         [FieldOffset(0x8A8)] public long InvitesPanel;
         [FieldOffset(0xA10)] public long ItemOnGroundTooltip;
-	[FieldOffset(0xAA0)] public long MapTabWindowStartPtr; //TODO: Find out what exactly this is...
+		[FieldOffset(0xAA0)] public long MapTabWindowStartPtr; //TODO: Find out what exactly this is...
         [FieldOffset(0x7E8)] public long RitualWindow;
+        [FieldOffset(0x7B0)] public long HarvestWindow;
+        [FieldOffset(0x7B8)] public long HorticraftWindow;
 	[FieldOffset(0x818)] public long DelveDarkessElem; 
     }
 }
