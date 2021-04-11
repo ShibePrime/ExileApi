8e32e1270c4255728a01159d65ec9657cbcbb9d9
MM <meyerm1@gmail.com>
2021-04-12 00:11:44 +0200

    Add some required API for CoPilot

---
+++ GameOffsets/SkillUiStateOffsets.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GameOffsets
+{
+	[StructLayout(LayoutKind.Explicit, Pack = 1)]
+	public struct SkillUiStateOffsets
+	{
+		[FieldOffset(0x10)]
+		public long CooldownLow;
+
+		[FieldOffset(0x18)]
+		public long CooldownHigh;
+
+		[FieldOffset(0x30)]
+		public int NumberOfUses;
+
+		[FieldOffset(0x3C)]
+		public ushort SkillId;
+	}
+}
