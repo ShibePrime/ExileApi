4b4c01829c68b57444387a7618958f09ac4701bc
MM <meyerm1@gmail.com>
2021-04-17 10:43:23 +0200

    3.14 WIP

--- Core/PoEMemory/MemoryObjects/ActorSkill.cs
+++ Core/PoEMemory/MemoryObjects/ActorSkill.cs
@@ -121,8 +121,8 @@ namespace ExileCore.PoEMemory.MemoryObjects
 
         long GetSkillUiStatePtr()
         {
-	        var listStart = M.Read<long>(pTheGame.IngameState.ServerData.Address + 0x8088, 0x570, 0x28, 0x170);
-	        var listEnd = M.Read<long>(pTheGame.IngameState.ServerData.Address + 0x8088, 0x570, 0x28, 0x178);
+	        var listStart = M.Read<long>(pTheGame.IngameState.ServerData.Address + 0x8088, 0x610, 0x28, 0x170);
+	        var listEnd = M.Read<long>(pTheGame.IngameState.ServerData.Address + 0x8088, 0x610, 0x28, 0x178);
 	        int maxCount = 100;
 	        for (var ptr = listStart; ptr < listEnd && maxCount > 0; ptr += 0x48, maxCount--)
 	        {
