81e90f9e46d1eaab6dca697d03fe3b3df627bcb3
Queuete <->
2021-04-17 03:45:32 +0200

    3.14 IngameSate and IngameUElements offsets

--- GameOffsets/IngameStateOffsets.cs
+++ GameOffsets/IngameStateOffsets.cs
@@ -5,23 +5,23 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct IngameStateOffsets
     {
-	    [FieldOffset(0x30)] public long Data;
-	    [FieldOffset(0x98)] public long IngameUi;
-		[FieldOffset(0xB8)] public long EntityLabelMap;
-	    [FieldOffset(0x418)] public long ServerData;
-	    [FieldOffset(0x540)] public long UIRoot;
-	    [FieldOffset(0x578)] public long UIHoverTooltip; //if this is a memory region, that tells if ANY tooltip is shown on the screen, then this offset is correct
-	    [FieldOffset(0x580)] public float CurentUElementPosX;
-	    [FieldOffset(0x584)] public float CurentUElementPosY;
-	    [FieldOffset(0x588)] public long UIHover; //telling different hovered objects
-	    [FieldOffset(0x5BC)] public float UIHoverX;
-	    [FieldOffset(0x5C0)] public float UIHoverY;
-	    [FieldOffset(0x5EC)] public float TimeInGame;
-	    [FieldOffset(0x5F4)] public float TimeInGameF;
-	    [FieldOffset(0x6F8)] public int DiagnosticInfoType;
-	    [FieldOffset(0x838)] public long LatencyRectangle;
-	    [FieldOffset(0xCD8)] public long FrameTimeRectangle;
-	    [FieldOffset(0xF28)] public long FPSRectangle;
-	    [FieldOffset(0x1080)] public int Camera;
+	    [FieldOffset(0x38)] public long Data;
+	    [FieldOffset(0xA0)] public long IngameUi;
+		[FieldOffset(0xC0)] public long EntityLabelMap;
+	    [FieldOffset(0x420)] public long ServerData;
+	    [FieldOffset(0x548)] public long UIRoot;
+	    [FieldOffset(0x580)] public long UIHoverTooltip; //if this is a memory region, that tells if ANY tooltip is shown on the screen, then this offset is correct
+	    [FieldOffset(0x588)] public float CurentUElementPosX;
+	    [FieldOffset(0x58C)] public float CurentUElementPosY;
+	    [FieldOffset(0x590)] public long UIHover; //telling different hovered objects
+	    [FieldOffset(0x5C4)] public float UIHoverX;
+	    [FieldOffset(0x5C8)] public float UIHoverY;
+	    [FieldOffset(0x5F4)] public float TimeInGame;
+	    [FieldOffset(0x5FC)] public float TimeInGameF;
+	    [FieldOffset(0x700)] public int DiagnosticInfoType;
+	    [FieldOffset(0x840)] public long LatencyRectangle;
+	    [FieldOffset(0xCE0)] public long FrameTimeRectangle;
+	    [FieldOffset(0xF30)] public long FPSRectangle;
+	    [FieldOffset(0x1088)] public int Camera;
     }
 }
--- GameOffsets/IngameUElementsOffsets.cs
+++ GameOffsets/IngameUElementsOffsets.cs
@@ -5,61 +5,61 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct IngameUElementsOffsets
     {
-        [FieldOffset(0x210)] public long GetQuests;
-        [FieldOffset(0x238)] public long GameUI;
-        [FieldOffset(0x370)] public long Mouse;
-        [FieldOffset(0x378)] public long SkillBar;
-        [FieldOffset(0x380)] public long HiddenSkillBar;
+        [FieldOffset(0x218)] public long GetQuests;
+        [FieldOffset(0x240)] public long GameUI;
+        [FieldOffset(0x378)] public long Mouse;
+        [FieldOffset(0x380)] public long SkillBar;
+        [FieldOffset(0x382)] public long HiddenSkillBar;
         //[FieldOffset(0x398)] public long HeistAlertPanel; // TODO: Verify
         //[FieldOffset(0x3B0)] public long PvpFightAnnouncePanel;
         //[FieldOffset(0x3C8)] public long PvpResultPanel;
         //[FieldOffset(0x3E0)] public long PvpSpectatingPanel;
         //[FieldOffset(0x3F0)] public long EditingHideoutNotifyPanel;
-        [FieldOffset(0x410)] public long ChatPanel;
+        [FieldOffset(0x418)] public long ChatPanel;
         //[FieldOffset(0x420)] public long BestiaryMissionCompletePanel;
         //[FieldOffset(0x428)] public long BestiaryNewBeastNotifyPanel;
         //[FieldOffset(0x430)] public long BestiaryNewRecipeNotifyPanel;
-        [FieldOffset(0x480)] public long QuestTracker;
+        [FieldOffset(0x488)] public long QuestTracker;
         //[FieldOffset(0x488)] public long BenchCraftNewRecipeNotifyPanel;
         //[FieldOffset(0x4A8)] public long HideoutUnlockNotifyPanel;
-        [FieldOffset(0x4F8)] public long OpenLeftPanel;
-        [FieldOffset(0x500)] public long OpenRightPanel;
+        [FieldOffset(0x500)] public long OpenLeftPanel;
+        [FieldOffset(0x508)] public long OpenRightPanel;
         //[FieldOffset(0x4F8)] public long OpenLeftPanel2; // Holds same address as above.
         //[FieldOffset(0x500)] public long OpenRightPanel2; // Holds same address as above.
         //[FieldOffset(0x518)] public long MtxShopPanel;
-        [FieldOffset(0x530)] public long InventoryPanel;
-        [FieldOffset(0x538)] public long StashElement;
+        [FieldOffset(0x538)] public long InventoryPanel;
+        [FieldOffset(0x540)] public long StashElement;
         //[FieldOffset(0x538)] public long GuildStashPanel;
         //[FieldOffset(0x540)] public long HideoutStashPanel; // Hideout stash panels from before rework.
         //[FieldOffset(0x548)] public long SocialPanel;
-        [FieldOffset(0x550)] public long TreePanel;
-        [FieldOffset(0x558)] public long AtlasPanel;
+        [FieldOffset(0x558)] public long TreePanel;
+        [FieldOffset(0x560)] public long AtlasPanel;
         //[FieldOffset(0x560)] public long CharacterPanel;
         //[FieldOffset(0x568)] public long OptionsPanel;
         //[FieldOffset(0x570)] public long ChallengesPanel;
         //[FieldOffset(0x578)] public long PantheonPanel;
         //[FieldOffset(0x580)] public long EventsPanel;
-        [FieldOffset(0x588)] public long WorldMap;
+        [FieldOffset(0x590)] public long WorldMap;
         //[FieldOffset(0x590)] public long MtxPanel;
         //[FieldOffset(0x598)] public long DecorationsPanel;
         //[FieldOffset(0x5A0)] public long HelpPanel;
-        [FieldOffset(0x5B8)] public long Map;
-        [FieldOffset(0x5c0)] public long itemsOnGroundLabelRoot;
+        [FieldOffset(0x5C0)] public long Map;
+        [FieldOffset(0x5C8)] public long itemsOnGroundLabelRoot;
         //[FieldOffset(0x5BB)] public long VisibleHealthBars; // TODO: Check if is only friendly or all bars.
-        [FieldOffset(0x5D0)] public long BanditDialog;
+        [FieldOffset(0x5D8)] public long BanditDialog;
         //[FieldOffset(0x630)] public long BuffPanel;
-        [FieldOffset(0x648)] public long NpcDialog;
-        [FieldOffset(0x650)] public long QuestRewardWindow;
-        [FieldOffset(0x668)] public long PurchaseWindow;
-        [FieldOffset(0x670)] public long SellWindow;
-        [FieldOffset(0x678)] public long TradeWindow;
+        [FieldOffset(0x650)] public long NpcDialog;
+        [FieldOffset(0x658)] public long QuestRewardWindow;
+        [FieldOffset(0x670)] public long PurchaseWindow;
+        [FieldOffset(0x678)] public long SellWindow;
+        [FieldOffset(0x680)] public long TradeWindow;
         //[FieldOffset(0x670)] public long MapReceptacle;
         //[FieldOffset(0x678)] public long PerandusCadiroOfferPanel;
         //[FieldOffset(0x680)] public long LabyrinthDivineFontPanel;
         //[FieldOffset(0x688)] public long TalismanStoneCirclePanel;
         //[FieldOffset(0x690)] public long TrialPlaquePanel;
         //[FieldOffset(0x698)] public long AscendancySelectPanel;
-        [FieldOffset(0x6A0)] public long MapDeviceWindow;
+        [FieldOffset(0x6A8)] public long MapDeviceWindow;
         //[FieldOffset(0x6A8)] public long DarkshrinePanel;
         //[FieldOffset(0x6B0)] public long BestiaryCraftPanel;
         //[FieldOffset(0x6B8)] public long LabyrinthPanel;
@@ -68,26 +68,26 @@ namespace GameOffsets
         //[FieldOffset(0x6D8)] public long FriendNoteEditorPanel;
         //[FieldOffset(0x6E0)] public long BetaLadderScreen;
         //[FieldOffset(0x6F0)] public long DivinationCardTradeScreen;
-        [FieldOffset(0x708)] public long IncursionWindow;
+        [FieldOffset(0x710)] public long IncursionWindow;
         //[FieldOffset(0x700)] public long IncursionCorruptionAltarPanel;
         //[FieldOffset(0x708)] public long IncursionAltarOfSacrificePanel;
         //[FieldOffset(0x710)] public long IncursionLapidaryLensPanel;
         //[FieldOffset(0x718)] public long NikoSubterraneanChartPanel; // This is the subterrranean chart when talking to niko.
-        [FieldOffset(0x720)] public long DelveWindow;  // This is the subterranean chart using in the mine.
-		    [FieldOffset(0x728)] public long ZanaMissionChoice;
+        [FieldOffset(0x728)] public long DelveWindow;  // This is the subterranean chart using in the mine.
+		[FieldOffset(0x730)] public long ZanaMissionChoice;
         //[FieldOffset(0x730)] public long SupportGemExamplesPanel;
         //[FieldOffset(0x738)] public long JunSyndicateInvestigationPanel; // this is the murder board when talking to Jun.
-        [FieldOffset(0x740)] public long BetrayalWindow; // this is the murder board shown during Betrayal encounter.
+        [FieldOffset(0x748)] public long BetrayalWindow; // this is the murder board shown during Betrayal encounter.
         //[FieldOffset(0x748)] public long HelenaHideoutSelectPanel;
-        [FieldOffset(0x750)] public long CraftBenchWindow;
-		    [FieldOffset(0x758)] public long UnveilWindow;
+        [FieldOffset(0x758)] public long CraftBenchWindow;
+		[FieldOffset(0x760)] public long UnveilWindow;
         //[FieldOffset(0x760)] public long BetrayalTrappedStashPanel;
         //[FieldOffset(0x768)] public long BetrayalTinysTrialPanel;
         //[FieldOffset(0x770)] public long BetrayalSyndicateCraftingBenchPanel;
         //[FieldOffset(0x778)] public long SynthesisSynthesiserPanel;
-        [FieldOffset(0x780)] public long SynthesisWindow;
+        [FieldOffset(0x788)] public long SynthesisWindow;
         //[FieldOffset(0x788)] public long CassiaAnointPanel;
-        [FieldOffset(0x790)] public long MetamorphWindow;  // This is the panel you encounter in maps.
+        [FieldOffset(0x798)] public long MetamorphWindow;  // This is the panel you encounter in maps.
         //[FieldOffset(0x798)] public long TanesLabMetamorphWindow; // This is the panel you encounter in Tane's Lab.
         //[FieldOffset(0x7A0)] public long HarvestCraftPanel;
         //[FieldOffset(0x7A8)] public long MetamorphPanel;
@@ -98,7 +98,7 @@ namespace GameOffsets
         //[FieldOffset(0x7D0)] public long HeistGrandHeistPanel;
         //[FieldOffset(0x7D8)] public long HeistLockerPanel;
         //[FieldOffset(0x810)] public long GuildPermissionsPanel;
-        [FieldOffset(0x830)] public long AreaInstanceUi;
+        [FieldOffset(0x838)] public long AreaInstanceUi;
         //[FieldOffset(0x840)] public long HeistLeaveNotifyPanel;
         //[FieldOffset(0x848)] public long MysteryBoxPanel;
         //[FieldOffset(0x850)] public long MtxSalvagePanel;
@@ -106,7 +106,7 @@ namespace GameOffsets
         //[FieldOffset(0x8E0)] public long SummonTanePanel;
         //[FieldOffset(0x8F8)] public long HideoutMusicPanel;
         //[FieldOffset(0x908)] public long ZoneTravelNotifyPanel;
-        [FieldOffset(0x990)] public long GemLvlUpPanel;
+        [FieldOffset(0x998)] public long GemLvlUpPanel;
         //[FieldOffset(0x9B8)] public long RampagePanel;
         //[FieldOffset(0x9C0)] public long IncursionProgressPanel;
         //[FieldOffset(0x9C8)] public long ZeroMustSurvivePanel;
@@ -114,13 +114,12 @@ namespace GameOffsets
         //[FieldOffset(0x9E0)] public long SynthesisRewardNotifyPanel;
         //[FieldOffset(0x9E8)] public long BlightProgressPanel;
         //[FieldOffset(0xA00)] public long HeistNotifyPanel;
-        [FieldOffset(0x8A8)] public long InvitesPanel;
-        [FieldOffset(0xA10)] public long ItemOnGroundTooltip;
-		    [FieldOffset(0xAA0)] public long MapTabWindowStartPtr; //TODO: Find out what exactly this is...
-        [FieldOffset(0x7E8)] public long RitualWindow;
-        [FieldOffset(0x7B0)] public long HarvestWindow;
-        [FieldOffset(0x7B8)] public long HorticraftWindow;
-	      [FieldOffset(0x818)] public long DelveDarkness; 
-
+        [FieldOffset(0x8B0)] public long InvitesPanel;
+        [FieldOffset(0xA18)] public long ItemOnGroundTooltip;
+		[FieldOffset(0xAA8)] public long MapTabWindowStartPtr; //TODO: Find out what exactly this is...
+        [FieldOffset(0x7F0)] public long RitualWindow;
+        [FieldOffset(0x7B8)] public long HarvestWindow;
+        [FieldOffset(0x7C0)] public long HorticraftWindow;
+	    [FieldOffset(0x820)] public long DelveDarkness; 
     }
 }
