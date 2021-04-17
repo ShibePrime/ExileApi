b25b96eb1d680169f79f315417596926d65ae618
Queuete <->
2021-04-17 12:42:00 +0200

    Fix render component

--- GameOffsets/RenderComponentOffsets.cs
+++ GameOffsets/RenderComponentOffsets.cs
@@ -7,10 +7,10 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct RenderComponentOffsets
     {
-        [FieldOffset(0x80)] public Vector3 Pos;
-        [FieldOffset(0x8C)] public Vector3 Bounds;
-        [FieldOffset(0xA0)] public NativeStringU Name;
-        [FieldOffset(0xC0)] public Vector3 Rotation;
-        [FieldOffset(0xE8)] public float Height;
+        [FieldOffset(0x88)] public Vector3 Pos;
+        [FieldOffset(0x94)] public Vector3 Bounds;
+        [FieldOffset(0xA8)] public NativeStringU Name;
+        [FieldOffset(0xCC)] public Vector3 Rotation;
+        [FieldOffset(0xF0)] public float Height;
 	}
 }
