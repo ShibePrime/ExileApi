89f3490c2289ba5d7d7e7e937dcd29e0e9aed3f9
Queuete <->
2021-04-18 13:38:13 +0200

    Add required using statement

--- Core/PoEMemory/Components/MinimapIcon.cs
+++ Core/PoEMemory/Components/MinimapIcon.cs
@@ -1,4 +1,5 @@
-﻿using ExileCore.Shared.Cache;
+﻿using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared.Cache;
 using ExileCore.Shared.Helpers;
 using GameOffsets;
 using GameOffsets.Native;
@@ -11,8 +12,6 @@ namespace ExileCore.PoEMemory.Components
         private MinimapIconOffsets MinimapIconOffsets => cachedValue.Value;
         public bool IsVisible => MinimapIconOffsets.IsVisible == 0; //M.Read<byte>(Address + 0x30) == 0;
         public bool IsHide => MinimapIconOffsets.IsHide == 1; //M.Read<byte>(Address + 0x34) == 1;
-        //NativeStringU Test =>M.Read<NativeStringU>(Address + 0x28, 0);
-        //NativeStringU Test => M.Read<NativeStringU>(MinimapIconOffsets.NamePtr);
         public string Name => NativeStringReader.ReadString(MinimapIconOffsets.NamePtr, M); 
         protected override void OnAddressChange() 
         {
