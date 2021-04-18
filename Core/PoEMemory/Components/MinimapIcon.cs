fe1907d1579f1ce8f7fd81deb1193bf6193c4028
wlastas <wlastas@mail.ru>
2021-04-18 03:38:43 +0300

    Update MinimapIcon.cs

--- Core/PoEMemory/Components/MinimapIcon.cs
+++ Core/PoEMemory/Components/MinimapIcon.cs
@@ -11,11 +11,10 @@ namespace ExileCore.PoEMemory.Components
         private MinimapIconOffsets MinimapIconOffsets => cachedValue.Value;
         public bool IsVisible => MinimapIconOffsets.IsVisible == 0; //M.Read<byte>(Address + 0x30) == 0;
         public bool IsHide => MinimapIconOffsets.IsHide == 1; //M.Read<byte>(Address + 0x34) == 1;
-        private NativeStringU Test => M.Read<NativeStringU>(MinimapIconOffsets.NamePtr); //M.Read<NativeStringU>(Address + 0x28, 0);
-        public string TestString => Test.ToString(M);
-        public string Name => Cache.StringCache.Read($"{MinimapIconOffsets.NamePtr}{Address}", () => TestString);
-
-        protected override void OnAddressChange()
+        //NativeStringU Test =>M.Read<NativeStringU>(Address + 0x28, 0);
+        //NativeStringU Test => M.Read<NativeStringU>(MinimapIconOffsets.NamePtr);
+        public string Name => NativeStringReader.ReadString(MinimapIconOffsets.NamePtr, M); 
+        protected override void OnAddressChange() 
         {
             cachedValue = new FrameCache<MinimapIconOffsets>(() => M.Read<MinimapIconOffsets>(Address));
         }
