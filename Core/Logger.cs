0875863b09f7df95f8942f24d9cb94b0c2492bb5
Queuete <->
2021-04-25 16:59:48 +0200

    Limit logging to single (verbose) file

--- Core/Logger.cs
+++ Core/Logger.cs
@@ -8,19 +8,11 @@ namespace ExileCore
     {
         private static ILogger _instance;
         public static ILogger Log =>
-            _instance ?? (_instance = new LoggerConfiguration().MinimumLevel
-                .ControlledBy(new LoggingLevelSwitch(LogEventLevel.Verbose)).WriteTo
-                .Logger(l => l.Filter.ByIncludingOnly(
-                        e => e.Level == LogEventLevel.Information).WriteTo
-                    .File(@"Logs\Info-.log", rollingInterval: RollingInterval.Day)).WriteTo
-                .Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Debug)
-                    .WriteTo.File(@"Logs\Debug-.log", rollingInterval: RollingInterval.Day)).WriteTo
-                .Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning)
-                    .WriteTo.File(@"Logs\Warning-.log", rollingInterval: RollingInterval.Day)).WriteTo
-                .Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error)
-                    .WriteTo.File(@"Logs\Error-.log", rollingInterval: RollingInterval.Day)).WriteTo
-                .Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Fatal)
-                    .WriteTo.File(@"Logs\Fatal-.log", rollingInterval: RollingInterval.Day)).WriteTo
-                .File(@"Logs\Verbose-.log", rollingInterval: RollingInterval.Day).CreateLogger());
+            _instance ?? (_instance = new LoggerConfiguration()
+                .MinimumLevel
+                .ControlledBy(new LoggingLevelSwitch(LogEventLevel.Verbose))
+                .WriteTo
+                .File(@"Logs\Verbose-.log", rollingInterval: RollingInterval.Day).CreateLogger()
+            );
     }
 }
