14bdbe80dcde8cfdc863fcbedea37363358b93fc
Snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2021-04-26 02:39:05 -0700

    More offsets and fixes to support features of MapNotify.

--- Core/Shared/Enums/InventoryTabMapSeries.cs
+++ Core/Shared/Enums/InventoryTabMapSeries.cs
@@ -2,11 +2,20 @@ namespace ExileCore.Shared.Enums
 {
     public enum InventoryTabMapSeries : byte
     {
-        Atlas_of_Worlds = 3,
         None = 0,
-        Original = 1,
-        The_Awakening = 2,
-        War_for_the_Atlas = 4,
-        Bestiary = 5
+        Original,
+        The_Awakening,
+        Atlas_of_Worlds,
+        War_for_the_Atlas,
+        Betrayal,
+        Synthesis,
+        Legion,
+        Blight,
+        Metamorph,
+        Delirium,
+        Harvest,
+        Heist,
+        Ritual,
+        Ultimatum
     }
 }
