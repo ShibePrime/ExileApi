f715c51df191cd2058d76c473b0bbb8d391965e2
Queuete <62324681+Queuete@users.noreply.github.com>
2021-05-16 22:28:32 +0200

    Use one thread per plugin

---
+++ Core/Threads/ThreadManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExileCore.Threads
+{
+    public class ThreadManager
+    {
+        private readonly ConcurrentDictionary<string, ThreadUnit> _threads;
+
+        public ThreadManager()
+        {
+            _threads = new ConcurrentDictionary<string, ThreadUnit>();
+        }
+
+        public bool AddOrUpdateJob(string name, Job job)
+        {
+            if (!_threads.ContainsKey(name))
+            {
+                var threadUnit = new ThreadUnit(name);
+                _threads.AddOrUpdate(name, threadUnit, (key, oldValue) => threadUnit);
+            }
+            if (_threads[name].Job != null && !_threads[name].Job.IsCompleted) return false;
+
+            _threads[name].Job = job;
+            return true;
+        }
+
+        public void AbortLongRunningThreads(int criticalMs)
+        {
+            foreach (var thread in _threads)
+            {
+                var job = thread.Value?.Job;
+                if (job == null) continue;
+                if (job.ElapsedMs < criticalMs) continue;
+
+                thread.Value.Abort();
+                DebugWindow.LogError($"ThreadManager -> Thread aborted jobname: {thread.Key}, after {job.ElapsedMs}");
+            }
+        }
+    }
+}
---
+++ Core/Threads/ThreadUnit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExileCore.Threads
+{
+    public class ThreadUnit
+    {
+        public Job Job { get; set; }
+        private readonly Thread _thread;
+        public ThreadUnit(string name)
+        {
+            _thread = new Thread(DoWork);
+            _thread.Name = name;
+            _thread.IsBackground = true;
+            _thread.Start();
+        }
+
+        private void DoWork()
+        {
+            while(true)
+            {
+                if (Job == null || Job.IsCompleted)
+                {
+                    Thread.Sleep(10);
+                    continue;
+                }
+                Job.Run();
+            }
+        }
+
+        public void Abort()
+        {
+            try
+            {
+                Job.IsFailed = true;
+                Job.IsCompleted = true;
+            }
+            finally
+            {
+                _thread.Abort();
+            }
+        }
+    }
+}
