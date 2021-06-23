3fea6475c1619c26df584876a2bd5183b30578bb
Jesse Granger <jesse_granger@fastmail.com>
2021-06-23 12:09:36 -0700

    Fix CurrVaalSouls Offset

--- Core/PoEMemory/MemoryObjects/ActorVaalSkill.cs
+++ Core/PoEMemory/MemoryObjects/ActorVaalSkill.cs
@@ -9,8 +9,8 @@ namespace ExileCore.PoEMemory.MemoryObjects
         private const int SKILL_NAME_OFFSET = 0x18;
         private const int ICON_OFFSET = 0x20;
         private const int MAX_VAAL_SOULS_OFFSET = 0x10;
-        private const int VAAL_SOULS_PER_USE_OFFSET = 0x14;
-        private const int CURRENT_VAAL_SOULS_OFFSET = 0x18;
+        private const int VAAL_SOULS_PER_USE_OFFSET = 0x14; // TODO: 0x14 is not correct here.
+        private const int CURRENT_VAAL_SOULS_OFFSET = 0x14;
         public string VaalSkillInternalName => M.ReadStringU(M.Read<long>(M.Read<long>(Address + NAMES_POINTER_OFFSET) + INTERNAL_NAME_OFFSET));
         public string VaalSkillDisplayName => M.ReadStringU(M.Read<long>(M.Read<long>(Address + NAMES_POINTER_OFFSET) + NAME_OFFSET));
         public string VaalSkillDescription => M.ReadStringU(M.Read<long>(M.Read<long>(Address + NAMES_POINTER_OFFSET) + DESCRIPTION_OFFSET));
