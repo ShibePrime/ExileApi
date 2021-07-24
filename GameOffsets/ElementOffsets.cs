10f48dc9c9c02bae7032d679ba93022256e3e5be
Queuete <62324681+Queuete@users.noreply.github.com>
2021-07-24 11:01:21 +0200

    IngameState, IngameData and Element offsets for 3.15

--- GameOffsets/ElementOffsets.cs
+++ GameOffsets/ElementOffsets.cs
@@ -9,31 +9,31 @@ namespace GameOffsets
     {
         public const int OffsetBuffers = 0x6EC;
 
-        [FieldOffset(0x18)] public long SelfPointer; // Useful for valid check
-        [FieldOffset(0x38)] public long ChildStart;
-        [FieldOffset(0x38)] public NativePtrArray Childs;
-        [FieldOffset(0x40)] public long ChildEnd;
-        [FieldOffset(0x88)] public long Root;
-        [FieldOffset(0x90)] public long Parent; // Works for Items only.
-        [FieldOffset(0x98)] public Vector2 Position;
-        [FieldOffset(0x98)] public float X;
-        [FieldOffset(0x9C)] public float Y;
-        [FieldOffset(0x108)] public float Scale;
-        [FieldOffset(0x111)] public byte IsVisibleLocal;
+        [FieldOffset(0x20)] public long SelfPointer; // Useful for valid check
+        [FieldOffset(0x40)] public long ChildStart;
+        [FieldOffset(0x40)] public NativePtrArray Childs;
+        [FieldOffset(0x48)] public long ChildEnd;
+        [FieldOffset(0x90)] public long Root;
+        [FieldOffset(0x98)] public long Parent; // Works for Items only.
+        [FieldOffset(0xA0)] public Vector2 Position;
+        [FieldOffset(0xA0)] public float X;
+        [FieldOffset(0xA4)] public float Y;
+        [FieldOffset(0x110)] public float Scale;
+        [FieldOffset(0x119)] public byte IsVisibleLocal;
 
-        [FieldOffset(0x110)] public uint ElementBorderColor;
-        [FieldOffset(0x114)] public uint ElementBackgroundColor;
-        [FieldOffset(0x118)] public uint ElementOverlayColor;
+        [FieldOffset(0x118)] public uint ElementBorderColor;
+        [FieldOffset(0x11C)] public uint ElementBackgroundColor;
+        [FieldOffset(0x120)] public uint ElementOverlayColor;
 
-        [FieldOffset(0x130)] public float Width;
-        [FieldOffset(0x134)] public float Height;
-        [FieldOffset(0x178)] public bool isHighlighted; // Checks B Channel of Border (#00000000 to #E7B478FF highlighted)
+        [FieldOffset(0x138)] public float Width;
+        [FieldOffset(0x13C)] public float Height;
+        [FieldOffset(0x180)] public bool isHighlighted; // Checks B Channel of Border (#00000000 to #E7B478FF highlighted)
 
-        [FieldOffset(0x178)] public uint TextBoxBorderColor;
-        [FieldOffset(0x17C)] public uint TextBoxBackgroundColor;
-        [FieldOffset(0x180)] public uint TextBoxOverlayColor;
+        [FieldOffset(0x180)] public uint TextBoxBorderColor;
+        [FieldOffset(0x180)] public uint TextBoxBackgroundColor;
+        [FieldOffset(0x188)] public uint TextBoxOverlayColor;
 
-        [FieldOffset(0x338)] public long Tooltip;
+        [FieldOffset(0x340)] public long Tooltip;
 
         //[FieldOffset(0x3CB)] public byte isShadow; // 0
         //[FieldOffset(0x3C9)] public byte isShadow2; // 1
