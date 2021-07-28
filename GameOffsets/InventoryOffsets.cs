8b3525c938c574431c8161f55ce8139e764995d3
0xE0D59 <0xE0D59@gmail.com>
2021-07-28 16:38:21 +0200

    3.15 InventoryOffsets

--- GameOffsets/InventoryOffsets.cs
+++ GameOffsets/InventoryOffsets.cs
@@ -5,14 +5,14 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct InventoryOffsets
     {
-        [FieldOffset(0x220)] public int MoveItemHoverState;
-        [FieldOffset(0x228)] public long HoverItem;
-        [FieldOffset(0x230)] public int XFake;
-        [FieldOffset(0x234)] public int YFake;
-        [FieldOffset(0x238)] public int XReal;
-        [FieldOffset(0x23C)] public int YReal;
-        [FieldOffset(0x248)] public int CursorInInventory;
-        [FieldOffset(0x390)] public long ItemCount;
-        [FieldOffset(0x478)] public int TotalBoxesInInventoryRow;
+        [FieldOffset(0x228)] public int MoveItemHoverState;
+        [FieldOffset(0x230)] public long HoverItem;
+        [FieldOffset(0x238)] public int XFake;
+        [FieldOffset(0x23C)] public int YFake;
+        [FieldOffset(0x240)] public int XReal;
+        [FieldOffset(0x244)] public int YReal;
+        [FieldOffset(0x250)] public int CursorInInventory;
+        [FieldOffset(0x3D8)] public long ItemCount;
+        [FieldOffset(0x4C0)] public int TotalBoxesInInventoryRow;
     }
 }
