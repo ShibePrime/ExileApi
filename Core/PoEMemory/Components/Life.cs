19b5c496f95b3a8e17c851b41a6fb57a56fd786a
Jesse Granger <jesse_granger@fastmail.com>
2021-11-17 21:06:01 -0800

    Reserved HP is stored internally as two fields, which must be combined.

--- Core/PoEMemory/Components/Life.cs
+++ Core/PoEMemory/Components/Life.cs
@@ -22,15 +22,18 @@ namespace ExileCore.PoEMemory.Components
         public int MaxHP => Address != 0 ? LifeComponentOffsetsStruct.MaxHP : 1;
         public int CurHP => Address != 0 ? LifeComponentOffsetsStruct.CurHP : 0;
         public double ReservedPercentHP => LifeComponentOffsetsStruct.ReservedPercentHP / 100d;
-        public int ReservedFlatHP => (int)Math.Round(MaxHP * ReservedPercentHP / 100);
+        public int ReservedFlatHP => Address != 0 ? LifeComponentOffsetsStruct.ReservedFlatHP : 0;
+        public int TotalReservedHP => Address == 0 ? 0 :
+            (int)(LifeComponentOffsetsStruct.ReservedFlatHP + (LifeComponentOffsetsStruct.MaxHP * (LifeComponentOffsetsStruct.ReservedPercentHP / 10000d)));
         public int MaxMana => Address != 0 ? LifeComponentOffsetsStruct.MaxMana : 1;
         public int CurMana => Address != 0 ? LifeComponentOffsetsStruct.CurMana : 1;
-        public double ReservedPercentMana => LifeComponentOffsetsStruct.ReservedPercentMana / 100d;
+        public double ReservedPercentMana => Address != 0 ? LifeComponentOffsetsStruct.ReservedPercentMana / 100d : 0d;
         public int ReservedFlatMana => (int)Math.Round(MaxMana * ReservedPercentMana / 100);
         public int MaxES => LifeComponentOffsetsStruct.MaxES;
         public int CurES => LifeComponentOffsetsStruct.CurES;
         public float HPPercentage => CurHP / (float) (MaxHP - ReservedFlatHP);
         public float MPPercentage => CurMana / (float) (MaxMana - ReservedFlatMana);
         public float ESPercentage => MaxES == 0 ? 0 : CurES / (float) MaxES;
+
     }
 }
