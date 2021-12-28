0617e883dc175f67fc9d79259d2ffef621f60ebb
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2021-12-28 06:56:16 -0800

    Update Sockets.cs
    Fixed lifetime issue related to List<SocketedGem> collecting SocketedGem by ref instead of by value.
    
    Users can now access the socket gems of an item.

--- Core/PoEMemory/Components/Sockets.cs
+++ Core/PoEMemory/Components/Sockets.cs
@@ -146,7 +146,7 @@ namespace ExileCore.PoEMemory.Components
             }
         }
 
-        public class SocketedGem
+        public struct SocketedGem
         {
             public Entity GemEntity;
             public int SocketIndex;
