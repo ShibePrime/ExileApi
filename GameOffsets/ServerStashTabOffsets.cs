226c1fb11773ef0bd18bf330a57e245e38b07304
SquirrelRat <37273964+SquirrelRat@users.noreply.github.com>
2022-02-19 12:59:35 -0600

    Add files via upload
    Updated InventoryID offset

--- GameOffsets/ServerStashTabOffsets.cs
+++ GameOffsets/ServerStashTabOffsets.cs
@@ -7,7 +7,7 @@ namespace GameOffsets
     public struct ServerStashTabOffsets
     {
         [FieldOffset(0x08)] public NativeStringU Name;
-        [FieldOffset(0x20)] public int InventoryId;
+        [FieldOffset(0x28)] public int InventoryId; //3.17.1
         [FieldOffset(0x26)] public ushort LinkedParentId;
         [FieldOffset(0x2C)] public uint Color;
         [FieldOffset(0x30)] public byte MemberFlags;
