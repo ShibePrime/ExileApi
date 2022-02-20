845088b1c2f1082c6e56c63798779419995cec83
SquirrelRat <37273964+SquirrelRat@users.noreply.github.com>
2022-02-19 20:24:55 -0600

    SellWindow Memory Update
    Way out of date. Plugins shouldn't have to workaround using GetChild, etc over and over.

--- Core/PoEMemory/MemoryObjects/SellWindow.cs
+++ Core/PoEMemory/MemoryObjects/SellWindow.cs
@@ -2,9 +2,9 @@ namespace ExileCore.PoEMemory.MemoryObjects
 {
     public class SellWindow : Element
     {
-        public Element SellDialog => GetChildAtIndex(3);
-        public Element YourOffer => SellDialog?.GetChildAtIndex(0);
-        public Element OtherOffer => SellDialog?.GetChildAtIndex(1);
+        public Element SellDialog => GetChildAtIndex(4);
+        public Element YourOffer => SellDialog?.GetChildAtIndex(1);
+        public Element OtherOffer => SellDialog?.GetChildAtIndex(0);
         public string NameSeller => SellDialog?.GetChildAtIndex(2).Text;
         public Element AcceptButton => SellDialog?.GetChildAtIndex(5);
         public Element CancelButton => SellDialog?.GetChildAtIndex(6);
