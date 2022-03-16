76f9f659d7bd3cbf12ca1b3e4397f029e1232ebf
Sarah <sr2449.sr2449@gmail.com>
2022-03-16 02:09:32 -0700

    Updated TerrainData offsets

--- GameOffsets/TerrainData.cs
+++ GameOffsets/TerrainData.cs
@@ -6,10 +6,10 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct TerrainData
     {
-        [FieldOffset(0x18)] public long NumCols;
-        [FieldOffset(0x20)] public long NumRows;
-        [FieldOffset(0xd8)] public NativePtrArray LayerMelee;
-        [FieldOffset(0xf0)] public NativePtrArray LayerRanged;
-        [FieldOffset(0x108)] public int BytesPerRow;
+        [FieldOffset(0x20)] public long NumCols;
+        [FieldOffset(0x28)] public long NumRows;
+        [FieldOffset(0xe0)] public NativePtrArray LayerMelee;
+        [FieldOffset(0xf8)] public NativePtrArray LayerRanged;
+        [FieldOffset(0x110)] public int BytesPerRow;
     }
 }
