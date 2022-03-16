8b8038af113f5566298f89d065650959a3527d2e
Sarah <sr2449.sr2449@gmail.com>
2022-03-15 19:47:07 -0700

    Updated IngameStateOffsets

--- GameOffsets/IngameStateOffsets.cs
+++ GameOffsets/IngameStateOffsets.cs
@@ -11,18 +11,18 @@ namespace GameOffsets
         [FieldOffset(0x78)] public long WorldData;
         [FieldOffset(0x98)] public long EntityLabelMap;
         [FieldOffset(0x1A8)] public long UIRoot; //3.16.3
-        [FieldOffset(0x438)] public long IngameUi; //3.16.3
         [FieldOffset(0x1E0)] public long UIHover; // element which is currently hovered
         [FieldOffset(0x1E8)] public Vector2 UIHoverPos; // top left corner of currently hovered UI element
         [FieldOffset(0x1F0)] public long UIHoverTooltip;
         [FieldOffset(0x218)] public Vector2i MousePos;
-        [FieldOffset(0x22C)] public Vector2 MousePosFloat;
         [FieldOffset(0x224)] public Vector2 UIHoverOffset; // mouse position offset in hovered UI element
+        [FieldOffset(0x22C)] public Vector2 MousePosFloat;
         [FieldOffset(0x3F8)] public float TimeInGameF; // time since last frame in seconds
         [FieldOffset(0x3FC)] public float TimeInGame; // total time in game in seconds
-        [FieldOffset(0x618)] public int DiagnosticInfoType;
-        [FieldOffset(0x848)] public long LatencyRectangle;
-        [FieldOffset(0xCE8)] public long FrameTimeRectangle;
-        [FieldOffset(0xF38)] public long FPSRectangle;
+        [FieldOffset(0x440)] public long IngameUi;
+        [FieldOffset(0x620)] public int DiagnosticInfoType;
+        [FieldOffset(0x850)] public long LatencyRectangle;
+        [FieldOffset(0xCF0)] public long FrameTimeRectangle;
+        [FieldOffset(0xF40)] public long FPSRectangle;
     }
 }
