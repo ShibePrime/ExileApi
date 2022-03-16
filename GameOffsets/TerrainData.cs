2642dc57fd502bc521f1ba9c49e38dcd6dd32793
Sarah <sr2449.sr2449@gmail.com>
2022-03-16 02:42:52 -0700

    Updated the TerrainData structure to be positioned at 0.

--- GameOffsets/TerrainData.cs
+++ GameOffsets/TerrainData.cs
@@ -6,10 +6,10 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct TerrainData
     {
-        [FieldOffset(0x20)] public long NumCols;
-        [FieldOffset(0x28)] public long NumRows;
-        [FieldOffset(0xe0)] public NativePtrArray LayerMelee;
-        [FieldOffset(0xf8)] public NativePtrArray LayerRanged;
-        [FieldOffset(0x110)] public int BytesPerRow;
+        [FieldOffset(0x0)] public long NumCols;
+        [FieldOffset(0x8)] public long NumRows;
+        [FieldOffset(0xc0)] public NativePtrArray LayerMelee;
+        [FieldOffset(0xd8)] public NativePtrArray LayerRanged;
+        [FieldOffset(0xF0)] public int BytesPerRow;
     }
 }
