ab1d9a0b367ed7deed4a115af8b50142dea3da4b
Sarah <sr2449.sr2449@gmail.com>
2022-03-16 03:20:30 -0700

    Updated offsets to align with CheatEngin's find out what acess this address. Figured since it's being updated this way by other people, it may be best for me to follow suit as well.

--- GameOffsets/TerrainData.cs
+++ GameOffsets/TerrainData.cs
@@ -6,10 +6,10 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct TerrainData
     {
-        [FieldOffset(0x0)] public long NumCols;
-        [FieldOffset(0x8)] public long NumRows;
-        [FieldOffset(0xc0)] public NativePtrArray LayerMelee;
-        [FieldOffset(0xd8)] public NativePtrArray LayerRanged;
-        [FieldOffset(0xF0)] public int BytesPerRow;
+        [FieldOffset(0x18)] public long NumCols;
+        [FieldOffset(0x20)] public long NumRows;
+        [FieldOffset(0xD8)] public NativePtrArray LayerMelee;
+        [FieldOffset(0xF0)] public NativePtrArray LayerRanged;
+        [FieldOffset(0x108)] public int BytesPerRow;
     }
 }
