e67d4b5026db6f2516f86061e6706035791bce73
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-04-21 15:49:53 -0700

    Update ServerInventoryOffsets.cs

--- GameOffsets/ServerInventoryOffsets.cs
+++ GameOffsets/ServerInventoryOffsets.cs
@@ -10,11 +10,11 @@ namespace GameOffsets
         [FieldOffset(0x140)] public byte IsRequested;
         [FieldOffset(0x144)] public int Columns;
         [FieldOffset(0x148)] public int Rows;
-        [FieldOffset(0x170)] public long InventoryItemsPtr;
+        [FieldOffset(0x168)] public long InventoryItemsPtr;
         [FieldOffset(0x180)] public long InventorySlotItemsPtr;
         [FieldOffset(0x188)] public long CountItems;
         [FieldOffset(0x188)] public int TotalItemsCount;
-        [FieldOffset(0x1F8)] public long Hash;          // Unknown
-        [FieldOffset(0x1E0)] public int ServerRequestCounter;    // Unknown, just a guess
+        [FieldOffset(0x1C8)] public int ServerRequestCounter;
+        [FieldOffset(0x1E8)] public long Hash;
     }
 }
