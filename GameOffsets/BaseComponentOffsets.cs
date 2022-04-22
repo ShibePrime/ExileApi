bd7284e1510ade3667089378a94463821e1e14ac
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-04-22 04:17:49 -0700

    Update BaseComponentOffsets.cs

--- GameOffsets/BaseComponentOffsets.cs
+++ GameOffsets/BaseComponentOffsets.cs
@@ -1,6 +1,4 @@
-using System.Net.Configuration;
 using System.Runtime.InteropServices;
-using GameOffsets.Native;
 
 namespace GameOffsets
 {
@@ -10,15 +8,16 @@ namespace GameOffsets
         [FieldOffset(0x10)] public byte ItemCellSizeX;
         [FieldOffset(0x11)] public byte ItemCellSizeY;
         [FieldOffset(0x18)] public long NameKey;
-        //[FieldOffset(0x38)] public long FlavourTextKey;
-        //[FieldOffset(0x60)] public long BaseItemsTypeKey;
-        //[FieldOffset(0x70)] public NativePtrArray TagKeys;
+        //[FieldOffset(0x38)] public long BaseItemsTypeKey;
+        //[FieldOffset(0x48)] public NativePtrArray TagKeys;
     }
 
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct BaseComponentOffsets
     {
         [FieldOffset(0x10)] public long BaseInfoKey;
+        //[FieldOffset(0x18)] public long ItemVisualIdentityKey;
+        //[FieldOffset(0x38)] public long FlavourTextKey;
         [FieldOffset(0x60)] public long PublicPricePtr;
         [FieldOffset(0xC6)] public byte InfluenceFlag;
         [FieldOffset(0xC7)] public byte isCorrupted;
