9e21306397b15a0573a153c7aba0f220de9f7fe6
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-04-21 18:28:15 -0700

    Update ChestComponentOffsets.cs

--- GameOffsets/ChestComponentOffsets.cs
+++ GameOffsets/ChestComponentOffsets.cs
@@ -5,10 +5,10 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct ChestComponentOffsets
     {
-        [FieldOffset(0x178)] public bool IsOpened;
-        [FieldOffset(0x179)] public bool IsLocked;
-        [FieldOffset(0x1B8)] public bool IsStrongbox;
-        [FieldOffset(0x17C)] public readonly byte quality;
         [FieldOffset(0x158)] public long StrongboxData;
+        [FieldOffset(0x160)] public bool IsOpened;
+        [FieldOffset(0x161)] public bool IsLocked;
+        [FieldOffset(0x164)] public readonly byte quality;
+        [FieldOffset(0x1A0)] public bool IsStrongbox;
     }
 }
