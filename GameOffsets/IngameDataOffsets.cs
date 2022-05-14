fd885004ac5d15687ae798b6785d6935f74027a4
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-05-13 21:05:24 -0700

    Update IngameDataOffsets.cs

--- GameOffsets/IngameDataOffsets.cs
+++ GameOffsets/IngameDataOffsets.cs
@@ -10,10 +10,10 @@ namespace GameOffsets
         [FieldOffset(0x104)] public uint CurrentAreaHash;
         [FieldOffset(0x118)] public NativePtrArray MapStats;
         [FieldOffset(0x188)] public long LabDataPtr;
-        [FieldOffset(0x670)] public long ServerData;
-        [FieldOffset(0x678)] public long LocalPlayer;
-        [FieldOffset(0x728)] public long EntityList;
-        [FieldOffset(0x730)] public long EntitiesCount;
-        [FieldOffset(0x8C0)] public TerrainData Terrain;
+        [FieldOffset(0x6F0)] public long ServerData;
+        [FieldOffset(0x6F8)] public long LocalPlayer;
+        [FieldOffset(0x7A8)] public long EntityList;
+        [FieldOffset(0x7B0)] public long EntitiesCount;
+        [FieldOffset(0x940)] public TerrainData Terrain;
     }
 }
