71e51f640942442c1eefb20f4e51f79bfd384772
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-05-25 00:13:09 -0700

    Update FragmentInventoryItem.cs

--- Core/PoEMemory/Elements/InventoryElements/FragmentInventoryItem.cs
+++ Core/PoEMemory/Elements/InventoryElements/FragmentInventoryItem.cs
@@ -1,17 +1,18 @@
-﻿using SharpDX;
-
 namespace ExileCore.PoEMemory.Elements.InventoryElements
 {
+    using SharpDX;
+
     public class FragmentInventoryItem : NormalInventoryItem
     {
         // Inventory Position in Currency Stash is always invalid.
         // Also, as items are fixed, so Inventory Position doesn't matter.
         public override int InventPosX => 0;
+
         public override int InventPosY => 0;
 
         public override RectangleF GetClientRect()
         {
-            return Parent.GetClientRect();
+            return this.Parent.GetClientRect();
         }
     }
 }
