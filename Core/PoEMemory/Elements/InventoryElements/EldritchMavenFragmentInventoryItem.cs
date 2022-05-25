cbbda2ecd2153009c7bf38119291faaec48ad281
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-05-25 00:11:34 -0700

    Added New Inventory Item overrides
    Flask and Gem Stash tabs. Fragment tab now includes a Eldritch/Maven tab.

---
+++ Core/PoEMemory/Elements/InventoryElements/EldritchMavenFragmentInventoryItem.cs
@@ -0,0 +1,21 @@
+﻿namespace ExileCore.PoEMemory.Elements.InventoryElements
+{
+    using SharpDX;
+
+    public class EldritchMavenFragmentInventoryItem : NormalInventoryItem
+    {
+        public override int InventPosX => base.InventPosX % 12;
+
+        public override RectangleF GetClientRect()
+        {
+            var baseDimensions = base.GetClientRect();
+            var parentDimensions = this.Parent.GetClientRectCache;
+
+            return new RectangleF(
+                parentDimensions.TopLeft.X + (this.InventPosX * baseDimensions.Width),
+                parentDimensions.TopLeft.Y,
+                baseDimensions.Width,
+                baseDimensions.Height);
+        }
+    }
+}
---
+++ Core/PoEMemory/Elements/InventoryElements/GemInventoryItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ExileCore.PoEMemory.Elements.InventoryElements
+{
+    using SharpDX;
+
+    public class GemInventoryItem : NormalInventoryItem
+    {
+        public override int InventPosX => 0;
+
+        public override int InventPosY => 0;
+
+        public override RectangleF GetClientRect()
+        {
+            return this.Parent.GetClientRect();
+        }
+    }
+}
