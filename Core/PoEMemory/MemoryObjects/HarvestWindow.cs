f7af79c63fadeeb6cfcd4ab9693c9849b32c253c
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-06-02 16:09:02 -0700

    Update HarvestWindow.cs
    Added inventory items, the current visible item (if one exists), and the craft button.

--- Core/PoEMemory/MemoryObjects/HarvestWindow.cs
+++ Core/PoEMemory/MemoryObjects/HarvestWindow.cs
@@ -1,12 +1,25 @@
-﻿using ExileCore.PoEMemory.Elements;
-using System.Collections.Generic;
-
 namespace ExileCore.PoEMemory.MemoryObjects
 {
+    using System.Collections.Generic;
+    using ExileCore.PoEMemory.Elements;
+    using ExileCore.PoEMemory.Elements.InventoryElements;
+
     public class HarvestWindow : Element
     {
+        public List<HarvestCraft> Crafts => this.TryGetChildFromIndices(out var craftsListPanel, 8, 0, 1)
+            ? craftsListPanel.GetChildrenAs<HarvestCraft>()
+            : new List<HarvestCraft>();
 
-        public List<HarvestCraft> Crafts => GetChildFromIndices(8, 0, 1).GetChildrenAs<HarvestCraft>();
+        public Element CraftButton => this.TryGetChildFromIndices(out var panel, 11, 1) ? panel : null;
 
+        public Entity Item => this.StashInventoryItemSlot?.Item ?? this.PlayerInventoryItemSlot?.Item;
+
+        public NormalInventoryItem PlayerInventoryItemSlot => this.TryGetChildFromIndices(out var itemSlotPanel, 11, 0, 0, 0, 1)
+            ? itemSlotPanel.AsObject<NormalInventoryItem>()
+            : null;
+
+        public NormalInventoryItem StashInventoryItemSlot => this.TryGetChildFromIndices(out var itemSlotPanel, 11, 0, 1, 0, 1)
+            ? itemSlotPanel.AsObject<NormalInventoryItem>()
+            : null;
     }
 }
