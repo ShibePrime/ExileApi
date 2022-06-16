947b6e26e1441e738b0f4bfc2bd8e7b7c1320640
Barragek0 <barragek0lol@gmail.com>
2022-06-16 03:10:44 +0100

    Update stash/inventory offsets for 3.18.1

--- GameOffsets/IngameUElementsOffsets.cs
+++ GameOffsets/IngameUElementsOffsets.cs
@@ -15,9 +15,9 @@ namespace GameOffsets
         [FieldOffset(0x4B8)] public long QuestTracker;
         [FieldOffset(0x546)] public long OpenLeftPanel;
         [FieldOffset(0x554)] public long OpenRightPanel;
-        [FieldOffset(0x578)] public long InventoryPanel;
-        [FieldOffset(0x550)] public long StashElement;
-        [FieldOffset(0x588)] public long GuildStashElement;
+        [FieldOffset(0x580)] public long InventoryPanel;
+        [FieldOffset(0x588)] public long StashElement;
+        [FieldOffset(0x590)] public long GuildStashElement; // offset is probably wrong, just bumped to 590 so stashelement could take its place
         [FieldOffset(0x628)] public long AtlasPanel;
         [FieldOffset(0x630)] public long AtlasSkillPanel;
         [FieldOffset(0x660)] public long WorldMap;
