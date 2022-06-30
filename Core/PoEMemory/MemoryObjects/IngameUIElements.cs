7be8f9fd3d4e6192c58fcb2abb5e837b2069ddee
Barragek0 <barragek0lol@gmail.com>
2022-06-30 03:52:41 +0100

    Update IngameUIElements.cs

--- Core/PoEMemory/MemoryObjects/IngameUIElements.cs
+++ Core/PoEMemory/MemoryObjects/IngameUIElements.cs
@@ -75,7 +75,7 @@ namespace ExileCore.PoEMemory.MemoryObjects
         public Element AtlasSkillPanel => GetObject<Element>(IngameUIElementsStruct.AtlasSkillPanel);
         public Map Map => _map ??= GetObject<Map>(IngameUIElementsStruct.Map);
         public Element MapDeviceWindow => GetObject<Element>(IngameUIElementsStruct.MapDeviceWindow);
-        public ItemsOnGroundLabelElement ItemsOnGroundLabelElement => GetObject<ItemsOnGroundLabelElement>(IngameUIElementsStruct.itemsOnGroundLabelRoot);
+        public ItemsOnGroundLabelElement ItemsOnGroundLabelElement => GetObject<ItemsOnGroundLabelElement>(IngameUIElementsStruct.ItemsOnGroundLabelElement);
         public IList<LabelOnGround> ItemsOnGroundLabels => ItemsOnGroundLabelElement.LabelsOnGround;
         public IList<LabelOnGround> ItemsOnGroundLabelsVisible => ItemsOnGroundLabelElement.LabelsOnGround.Where(x => x.Address != 0 && x.IsVisible).ToList();
         public GemLvlUpPanel GemLvlUpPanel => GetObject<GemLvlUpPanel>(IngameUIElementsStruct.GemLvlUpPanel);
